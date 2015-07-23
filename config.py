12da7236e744aa9a7d4a4fc2612a7a766d7c40b1
Ian Harvey <ian.harvey@digital.landregistry.gov.uk>
2015-07-23 14:43:56 +0100

    Add separate config for preview.

--- config.py
+++ config.py
@@ -1,9 +1,14 @@
 import os
 
+
 class Config(object):
     DEBUG = False
 
-class DevelopmentConfig(object):
+
+class DevelopmentConfig(Config):
     DEBUG = True
     AUTOMATION_IND = True
 
+
+class PreviewConfig(Config):
+    AUTOMATION_IND = True
